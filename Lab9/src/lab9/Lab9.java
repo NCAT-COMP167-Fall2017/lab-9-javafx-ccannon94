59fbd8cb73674852248ee36b100b980a69a8de35
Chris Cannon <chris.c.cannon@gmail.com>
2017-11-01 22:48:11 -0400

    Add eventhandler base code

--- Lab9/src/lab9/Lab9.java
+++ Lab9/src/lab9/Lab9.java
@@ -24,8 +24,9 @@ import javafx.stage.Stage;
  * @author CCannon
  */
 public class Lab9 extends Application {
+
     Button[][] buttons;
-    
+
     @Override
     public void start(Stage primaryStage) {
         int n = 0;
@@ -33,38 +34,41 @@ public class Lab9 extends Application {
         dialog.setTitle("Maze Configuration");
         dialog.setHeaderText("Each maze is an n x n square");
         dialog.setContentText("Please enter the dimension for n:");
-        
+
         Optional<String> result = dialog.showAndWait();
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             n = Integer.parseInt(result.get());
         }
-        
+
         GridPane grid = new GridPane();
         buttons = new Button[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 Button newButt = new Button();
                 newButt.setStyle("-fx-background-color: blue");
                 newButt.setPrefSize(50, 50);
-                
+                newButt.setOnAction(new MyHandler());
+
                 grid.add(newButt, j, i);
                 buttons[i][j] = newButt;
             }
         }
-        
+
         HBox commandPane = new HBox();
         Button saveButton = new Button("Save");
+        saveButton.setOnAction(new MyHandler());
         Button exitButton = new Button("Exit");
+        exitButton.setOnAction(new MyHandler());
         commandPane.getChildren().addAll(saveButton, exitButton);
         commandPane.setAlignment(Pos.CENTER);
-        
+
         BorderPane root = new BorderPane();
-        
+
         root.setCenter(grid);
         root.setBottom(commandPane);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -77,5 +81,48 @@ public class Lab9 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
+    //Returns the Maze character associated with the given button   
+    private char getColorChar(Button b) {
+        if (b.getStyle().indexOf("blue") > -1) {
+            return '0';
+        } else if (b.getStyle().indexOf("white") > -1) {
+            return '1';
+        } else if (b.getStyle().indexOf("green") > -1) {
+            return 'S';
+        } else {
+            return 'E';
+        }
+    }
+
+    //Inner class lister for buttons
+    public class MyHandler implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent e) {
+            //e.getSource() returns the button that was clicked.
+            Button b = (Button) e.getSource();
+
+            if (b.getText().equals("Save")) {
+                //create output file
+
+            } else if (b.getText().equals("Exit")) {
+                //exit the application
+
+            } else { //a maze button was clicked
+
+                char val = getColorChar(b);
+                if (val == '0') {  //the button is currently blue so change it to white
+                    b.setStyle("-fx-background-color: white;");
+                } else if (val == '1') {
+
+                } else if (val == 'S') {
+
+                } else {
+
+                }
+
+            }
+        }
+    }
 }
